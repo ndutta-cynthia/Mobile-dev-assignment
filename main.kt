1a200ca16bc344cffb05eca30c2ac606c106c459
ndutta-cynthia <nduttamaina@gmail.com>
2024-02-08 22:10:10 +0300

    added one file

---
+++ main.kt
@@ -0,0 +1,12 @@
+fun main(args: Array<String>) {
+    var fullName = "Cynthia Maina"
+    var age = 23
+    var phoneNumber = "+254712345678"
+    var weight = 59.9F
+    var kenyan = true
+    println(fullName)
+    println(age)
+    println(phoneNumber)
+    println(weight)
+    println(kenyan)
+}
